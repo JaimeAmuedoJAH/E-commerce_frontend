34d97a6e64823e50477655fee17a49a55b133ec0
JaimeAmuedoJAH <Jaimeamuedo96@gmail.com>
2026-04-29 12:42:41 +0200

    Feature: Añadida pagina de registro con confirmación de contraseña. #9

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
+import Registro from './pages/Registro'
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/registro" element={<Registro />} />
     </Routes>
   )
 }
